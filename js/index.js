101a947de02e0e256f293e71f55032016d8acf4c
Le Xuan Phuoc <stevenmarks147@gmail.com>
2023-08-03 17:59:46 +0700

    Load items index page

--- js/index.js
+++ js/index.js
@@ -3,20 +3,34 @@ $(document).ready(function () {
   let img;
   let span;
 
-  var links = document.querySelectorAll(".category-links");
-
+  // Load Categories
   $.ajax({
     url: "http://localhost:8080/category",
     method: "GET",
   }).done(function (response) {
+    loadAllProduct();
+    
     var categories = response.data;
-
+    
     for (var i = 0; i < categories.length; i++) {
       console.log(categories[i]);
     }
 
-    var test = document.getElementById("category");
+    var mainCategory = document.getElementById("category");
 
+    var link = document.createElement("a");
+    link.innerHTML = "All Products";
+    link.setAttribute(
+      "class",
+      "stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 category-feature how-active1"
+    );
+    link.addEventListener("click", function(){
+      activeCategory(this);
+      loadAllProduct();
+    });
+    mainCategory.appendChild(link);
+
+    
     for (var i = 0; i < categories.length; i++) {
       let id = categories[i].id;
       var element = document.createElement("a");
@@ -26,109 +40,159 @@ $(document).ready(function () {
         "stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 category-feature"
       );
       element.setAttribute("id", categories[i].id);
-      //element.setAttribute("onclick", "getProductByCategory(this)");
 
       element.addEventListener("click", function () {
-        $.ajax({
-          url: "http://localhost:8080/category/product?id=" + id,
-          method: "GET",
-        }).done(function (response) {
-
-          var data = response.data;
-
-          var itemDetail = document.getElementById("test");
-
-          for (var j = 0; j < data.length; j++) {
-
-            var quarter = document.createElement("div");
-            quarter.setAttribute("class", "col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women");
-
-            var rootElement = document.createElement("div");
-            rootElement.setAttribute("class", "block2");
-
-            // list products by category id
-            // upper side of product detail
-            div = document.createElement("div");
-            div.setAttribute("class", "block2-pic hov-img0");
-            // feature detail of div 1
-            img = document.createElement("img");
-            img.setAttribute("src", "images/product-06.jpg");
-            img.setAttribute("alt", "IMG-PRODUCT");
-
-            let link = document.createElement("a");
-            link.setAttribute(
-              "class",
-              "block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1"
-            );
-            link.setAttribute("href", "#");
-            link.innerHTML = "Quick View";
-
-            div.appendChild(img);
-            div.appendChild(link);
-            rootElement.appendChild(div);
-            quarter.appendChild(rootElement);
-
-            // lower side of product detail
-            let rootElement2 = document.createElement("div");
-            rootElement2.setAttribute(
-              "class",
-              "block2-txt flex-w flex-t p-t-14"
-            );
-
-            div = document.createElement("div");
-            div.setAttribute("class", "block2-txt-child1 flex-col-l");
-
-            link = document.createElement("a");
-            link.setAttribute(
-              "class",
-              "stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6"
-            );
-            link.setAttribute("href", "product-detail.html");
-            link.innerHTML = data[j].name;
-
-            span = document.createElement("span");
-            span.setAttribute("class", "stext-105 cl3");
-            span.innerHTML = data[j].price;
-
-            div.appendChild(link);
-            div.appendChild(span);
-            rootElement2.appendChild(div);
-
-            div = document.createElement("div");
-            div.setAttribute("class", "block2-txt-child2 flex-r p-t-3");
-            link = document.createElement("a");
-            link.setAttribute("class", "btn-addwish-b2 dis-block pos-relative js-addwish-b2");
-            link.setAttribute("href", "#");
-            img = document.createElement("img");
-            img.setAttribute("class", "icon-heart1 dis-block trans-04");  
-            img.setAttribute("src", "images/icons/icon-heart-01.png");
-            img.setAttribute("alt", "ICON");
-            link.appendChild(img);
-            img = document.createElement("img");
-            img.setAttribute("class", "icon-heart2 dis-block trans-04 ab-t-l");  
-            img.setAttribute("src", "images/icons/icon-heart-02.png");
-            img.setAttribute("alt", "ICON");
-            link.appendChild(img);
-            div.appendChild(link);
-            rootElement2.appendChild(div);  
-            quarter.appendChild(rootElement2);  
-
-            itemDetail.appendChild(quarter);
-            itemDetail.style="height: 32rem;";
-          }
-        });
+        
+        activeCategory(this);
+        
+        loadProductByCategory(id);
+        
       });
 
-      test.appendChild(element);
+      mainCategory.appendChild(element);
     }
   });
 
-  //   var categories = document.getElementsByClassName("category-feature");
-  //   categories.forEach(function () {
-  //     this.addEventListener("click", function () {
-  //       var id = this.getAttribute("id");
-  //       console.log(id);
-  //     });
-  //   });
-  //   console.log(categories);
+
+  function activeCategory(activeLink){
+    activeLink.style.color = "grey";
+    activeLink.style.pointerEvents = "none";
+    activeLink.setAttribute(
+    "class",
+    "stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 category-feature how-active1");
+
+    var links = document.querySelectorAll(".category-feature");
+    for(var i=0; i<links.length; i++){
+      if(links[i] != activeLink){
+        links[i].style.color = "black";
+        links[i].style.pointerEvents = "auto";
+        links[i].setAttribute(
+          "class",
+          "stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 category-feature");
+      }
+    }
+  }
+
+  function reload(){
+    var items = document.querySelectorAll(".isotope-item");
+    for(var i=0; i<items.length; i++){
+      items[i].remove();
+    }
+  }
+
+  function loadAllProduct(){
+    $.ajax({
+      url: "http://localhost:8080/product",
+      method: "GET",
+    }).done(function(response){ 
+      reload();
+
+      var items = response.data;
+
+      for (var j = 0; j < items.length; j++) {
+        createItemELement(items[j]);
+      }  
+    });
+  }
+
+  function loadProductByCategory(id){
+    $.ajax({
+      url: "http://localhost:8080/product/category/" + id,
+      method: "GET",
+    }).done(function (response) {
+      reload();
+
+      var items = response.data;
+
+      for (var j = 0; j < items.length; j++) {
+        createItemELement(items[j]);
+      }  
+    });
+  }
+
+  function createItemELement(data){
+        let itemDetail = document.getElementById("items-detail");
+
+        var quarter = document.createElement("div");
+        quarter.setAttribute("class", "col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women");
+        
+        let rootElement = document.createElement("div");
+        rootElement.setAttribute("class", "block2");
+
+        // list products by category id
+        // upper side of product detail
+        div = document.createElement("div");
+        div.setAttribute("class", "block2-pic hov-img0");
+        // feature detail of div 1
+        img = document.createElement("img");
+        img.setAttribute("src", data.image);
+        img.style.width = "20rem";
+        img.style.height = "25rem";
+        img.setAttribute("alt", "IMG-PRODUCT");
+
+        let link = document.createElement("a");
+        link.setAttribute(
+          "class",
+          "block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1"
+        );
+        link.setAttribute("href", "product-detail.html");
+        link.innerHTML = "Quick View";
+
+        div.appendChild(img);
+        div.appendChild(link);
+        rootElement.appendChild(div);
+
+        // lower side of product detail
+        let rootElement2 = document.createElement("div");
+        rootElement2.setAttribute(
+          "class",
+          "block2-txt flex-w flex-t p-t-14"
+        );
+
+        div = document.createElement("div");
+        div.setAttribute("class", "block2-txt-child1 flex-col-l");
+
+        link = document.createElement("a");
+        link.setAttribute(
+          "class",
+          "stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6"
+        );
+        link.setAttribute("href", "product-detail.html");
+        link.innerHTML = data.name;
+
+        span = document.createElement("span");
+        span.setAttribute("class", "stext-105 cl3");
+        span.style.color = "red";
+        span.innerHTML = "$" + data.price;
+
+        div.appendChild(link);
+        div.appendChild(span);
+        rootElement2.appendChild(div);
+
+        div = document.createElement("div");
+        div.setAttribute("class", "block2-txt-child2 flex-r p-t-3");
+        link = document.createElement("a");
+        link.setAttribute("class", "btn-addwish-b2 dis-block pos-relative js-addwish-b2");
+        link.setAttribute("href", "#");
+        img = document.createElement("img");
+        img.setAttribute("class", "icon-heart1 dis-block trans-04");  
+        img.setAttribute("src", "images/icons/icon-heart-01.png");
+        img.setAttribute("alt", "ICON");
+        link.appendChild(img);
+        img = document.createElement("img");
+        img.setAttribute("class", "icon-heart2 dis-block trans-04 ab-t-l");  
+        img.setAttribute("src", "images/icons/icon-heart-02.png");
+        img.setAttribute("alt", "ICON");
+        link.appendChild(img);
+        div.appendChild(link);
+        rootElement2.appendChild(div);
+          
+        rootElement.appendChild(rootElement2);
+        quarter.appendChild(rootElement);
+
+        console.log(quarter);
+        itemDetail.appendChild(quarter);
+        itemDetail.style.marginBottom = "60rem";
+  }
+
 });
--- js/login.js
+++ js/login.js
@@ -1,28 +1,34 @@
-$(document).ready(function() {
-    $("#btn-sign-in").click(function() {
-        var username = $("#user").val();
-        var password = $("#pass").val();
+$(document).ready(function () {
+  $("#btn-sign-in").click(function () {
+    var username = $("#user").val();
+    var password = $("#pass").val();
 
-        $.ajax({
-            url: "http://localhost:8080/signin",
-            method: "POST",
-            data: {
-                 email: username,
-                 password : password
-                } 
-        })
-        // khi goi API thi ket qua se tra o day
-        .done(function(response) {
-            var token = response.data;
-            if(token != null && token != ""){
-                // luu token vao bo nho cua browser
-                // "gioHang": [{id:1, title:"Shirt", price:109,soluong:10}]
-                localStorage.setItem("token", token);
-                window.location.href = "index.html";
-            }else{
-                alert("Sai tai khoan")
-            }
-        });
-        
+    $.ajax({
+      url: "http://localhost:8080/signin",
+      method: "POST",
+      data: {
+        email: username,
+        password: password,
+      },
+      timeout: 60000,
+      statusCode: {
+        403: function () {
+          alert("Sai tai khoan");
+        },
+      },
+    //   error: function (e) {
+    //         alert(e.responseText);
+    //     }
     })
-})
+      // khi goi API thi ket qua se tra o day
+      .done(function (response) {
+        var token = response.data;
+        if (token != null && token != "") {
+          // luu token vao bo nho cua browser
+          // "gioHang": [{id:1, title:"Shirt", price:109,soluong:10}]
+          localStorage.setItem("token", token);
+          window.location.href = "index.html";
+        }
+      });
+  });
+});
